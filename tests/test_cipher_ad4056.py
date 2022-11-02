dc02afb508be1a328c1364d7769c5ac55b827eb5
annid1008 <ad4056@columbia.edu>
2022-11-02 03:32:15 -0400

    add test

--- tests/test_cipher_ad4056.py
+++ tests/test_cipher_ad4056.py
@@ -1,1 +1,7 @@
 from cipher_ad4056 import cipher_ad4056
+
+def test_cipher():
+    expected = 'khoor'
+    actual = cipher_ad4056.cipher('hello', 3)
+    assert actual == expected, "Result shold be " + expected
+
